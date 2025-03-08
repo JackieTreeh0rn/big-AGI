c96564cf4f1c3192e43a6d6a098dc3aaca6f1b59
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-03-08 17:02:25 -0500

    ...

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -8,7 +8,7 @@ docker network create -d overlay --subnet=10.0.10.0/24 --gateway=10.0.10.1 big-a
 docker login -u admin -p admin13694587 registry.bitwisesolutions.co
 
 # Build
-docker buildx build --platform "linux/amd64,linux/arm64" -t registry.bitwisesolutions.co/big-agi:latest --no-cache --push .
+# docker buildx build --platform "linux/amd64,linux/arm64" -t registry.bitwisesolutions.co/big-agi:latest --no-cache --push .
 
 # Deploy
 docker stack deploy -c docker-compose-browserless-swarm.yaml big-agi
