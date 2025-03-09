97e9e979fa8371af119cfa92be88bacfe93ebca9
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-03-09 04:28:34 -0400

    ...

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -5,10 +5,10 @@ export ROOT_DOMAIN=bitwisesolutions.co #Traefik
 docker network create -d overlay --subnet=10.0.10.0/24 --gateway=10.0.10.1 big-agi
 
 # docker login
-# docker login -u admin -p admin13694587 registry.bitwisesolutions.co
+docker login -u admin -p admin13694587 registry.bitwisesolutions.co
 
 # Build
-# docker buildx build --platform "linux/amd64,linux/arm64" -t registry.bitwisesolutions.co/big-agi:latest --no-cache --push .
+docker buildx build --platform "linux/amd64,linux/arm64" -t registry.bitwisesolutions.co/big-agi:latest --push .
 
 # Deploy
 docker stack deploy -c docker-compose-browserless-swarm.yaml big-agi
