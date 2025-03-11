a1edd653345b749867dde810dd51653cfb7e09a3
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-03-11 00:48:25 -0400

    swarm update

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -5,7 +5,7 @@ export ROOT_DOMAIN=bitwisesolutions.co #Traefik
 docker network create -d overlay --subnet=10.0.10.0/24 --gateway=10.0.10.1 big-agi
 
 # docker login
-# docker login -u admin -p admin13694587 registry.bitwisesolutions.co
+# docker login -u admin registry.bitwisesolutions.co
 
 # Build
 # docker buildx build --platform "linux/amd64,linux/arm64" -t registry.bitwisesolutions.co/big-agi:latest --push . && \
