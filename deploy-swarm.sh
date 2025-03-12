d831042833d30c3408e191cefa5ebdb345b9cbdf
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-03-12 07:24:43 -0400

    deploy update

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -8,6 +8,6 @@ set -o allexport
 export PUID=$(id -u)
 export PGID=$(id -g)
 # Create Network (customize)
-docker network create -d overlay --subnet=10.0.10.0/24 --gateway=10.0.10.1 ${STACK_NAME}
+docker network create -d overlay --subnet=10.0.10.0/24 --gateway=10.0.10.1 ${BACKEND_NETWORK_NAME}
 # Deploy
 docker stack deploy -c docker-stack-compose.yml ${STACK_NAME}
