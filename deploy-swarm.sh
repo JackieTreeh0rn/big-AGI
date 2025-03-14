5c3b848505932f7801b4c85831f0b98f2b053872
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-03-14 15:17:05 -0400

    deploys

--- deploy-swarm.sh
+++ deploy-swarm.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-set -o allexport
-. ./.env
+# set -o allexport
+# . ./.env
 
+# Traefik
+export ROUTER_NAME="bigagi"
+export ROUTER_NAME_PUBLIC="bigagi-pub"
+export ROOT_DOMAIN="bitwisesolutions.co"
+
+# Docker
+export STACK_NAME="big-agi"
+export BACKEND_NETWORK_NAME=${STACK_NAME}_backend
 
 # Docker
 export PUID=$(id -u)
